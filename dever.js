7c7d00108cee3364ff783d951ce6f96fa7990f1c
Sofia2821 <91471346+Sofia2821@users.noreply.github.com>
2021-10-17 10:00:56 -0300

    Update dever.js

--- dever.js
+++ dever.js
@@ -1,11 +1,11 @@
-const subtitulos = document.querySectorAll('h1');
-      const topicos = new Array();
-      subtitulos.forEach(subtitulo => {
-        topicos.push(subtitulo.innerHTML);
-      });
-      lista = document.querySelector('ol');
-      topicos.forEach(topico => {
-        const item = document.createElement('li');
-        item.innerHTML = topico;
-        lista.append(item);
-      });  
+const subtitulos = document.querySelectorAll('h2');
+const topicos = new Array();
+        subtitulos.forEach(subtitulo => {
+            topicos.push(subtitulo.textContent);
+        });
+        const lista = document.querySelector('ol');
+        topicos.forEach(topico => {
+            const item = document.createElement('li');
+            item.innerHTML = '<a href="#' + topico + '">' + topico + '</a>';
+            lista.append(item);
+        });
