c191a45b933bf6cd21a5fe08bed7f3375f4e0e93
Sofia2821 <91471346+Sofia2821@users.noreply.github.com>
2021-12-04 14:58:13 -0300

    Update dever.js

--- dever.js
+++ dever.js
@@ -1,4 +1,15 @@
+document.addEventListener("DOMContentLoaded", inserir)
 let tamanho = 2;
+
+        function inserir(){
+			div = document.createElement("div");
+			div.style.fontSize = `${tamanho}em`;
+			div.innerHTML = `<button onclick="aumentatitulo()">+</button>
+            <button onclick="diminuititulo()">-</button>
+			`;
+			document.body.prepend(div)
+		}
+
         function atualizatamanho() {
             const h1 = document.querySelector("h1");
             h1.style.fontSize = `${tamanho}em`;
